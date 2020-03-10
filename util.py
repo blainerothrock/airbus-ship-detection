b9fdc5f79141f509334c3aba9ef005cba8cde5fd
Will <williamdong97@gmail.com>
2020-03-10 15:01:34 -0500

    contains all relevant functions

---
+++ util.py
@@ -0,0 +1,184 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras import models, Input
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D, UpSampling2D, GaussianNoise, BatchNormalization, concatenate, Cropping2D, ZeroPadding2D
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import tensorflow.keras.backend as K
+from skimage.io import imread
+from sklearn.model_selection import train_test_split
+
+from skimage.segmentation import mark_boundaries
+from skimage.util import montage
+montage_rgb = lambda x: np.stack([montage(x[:, :, :, i]) for i in range(x.shape[3])], -1)
+
+import os, shutil
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from PIL import Image
+import gc
+import tensorflow.keras.backend as K
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.losses import binary_crossentropy
+
+
+
+def agg_ship_count(seg):
+    seg['ship_count'] = seg['EncodedPixels'].map(lambda x: 1 if isinstance(x, str) else 0)
+    seg.sample(10)
+
+    # aggregate ship images and sum number of ships
+    unique_seg = seg.groupby('ImageId').agg({'ship_count': 'sum'}).reset_index()
+    unique_seg['file_size_kb'] = unique_seg['ImageId'].map(lambda c_img_id: os.stat(os.path.join(train_image_dir, c_img_id)).st_size/1024 if os.path.exists(os.path.join(train_image_dir, c_img_id)) else 0.0)
+    unique_seg = unique_seg[unique_seg['file_size_kb']>50]
+    unique_seg.drop(['file_size_kb'], axis=1, inplace=True)
+
+    unique_seg['has_ship'] = unique_seg['ship_count'].map(lambda x: 1 if x > 0.0 else 0)
+    unique_seg['class'] = unique_seg['ship_count'].map(lambda x: 'ship' if x > 0.0 else 'no_ship')
+
+    return unique_seg
+
+def plotImages(images_arr):
+    fig, axes = plt.subplots(1, 5, figsize=(20,20))
+    axes = axes.flatten()
+    for img, ax in zip( images_arr, axes):
+        ax.imshow(img)
+        ax.axis('off')
+    plt.tight_layout()
+    plt.show()
+
+    # Helpers for decoding the masks
+# From: https://www.kaggle.com/paulorzp/run-length-encode-and-decode
+from skimage.morphology import label
+def multi_rle_encode(img):
+    labels = label(img[:, :, 0])
+    return [rle_encode(labels==k) for k in np.unique(labels[labels>0])]
+
+def rle_encode(img):
+    '''
+    img: numpy array, 1 - mask, 0 - background
+    Returns run length as string formated
+    '''
+    pixels = img.T.flatten()
+    pixels = np.concatenate([[0], pixels, [0]])
+    runs = np.where(pixels[1:] != pixels[:-1])[0] + 1
+    runs[1::2] -= runs[::2]
+    return ' '.join(str(x) for x in runs)
+
+def rle_decode(mask_rle, shape=(768, 768)):
+    '''
+    mask_rle: run-length as string formated (start length)
+    shape: (height,width) of array to return
+    Returns numpy array, 1 - mask, 0 - background
+    '''
+    s = mask_rle.split()
+    starts, lengths = [np.asarray(x, dtype=int) for x in (s[0:][::2], s[1:][::2])]
+    starts -= 1
+    ends = starts + lengths
+    img = np.zeros(shape[0]*shape[1], dtype=np.uint8)
+    for lo, hi in zip(starts, ends):
+        img[lo:hi] = 1
+    return img.reshape(shape).T  # Needed to align to RLE direction
+
+def masks_as_image(in_mask_list):
+    # Take the individual ship masks and create a single mask array for all ships
+    all_masks = np.zeros((768, 768), dtype = np.int16)
+    #if isinstance(in_mask_list, list):
+    for mask in in_mask_list:
+        if isinstance(mask, str):
+            all_masks += rle_decode(mask)
+    return np.expand_dims(all_masks, -1)
+
+    # custom image generator
+def make_image_gen(in_df, params = unet_params):
+    all_batches = list(in_df.groupby('ImageId'))
+    out_rgb = []
+    out_mask = []
+    while True:
+        np.random.shuffle(all_batches)
+        for c_img_id, c_masks in all_batches:
+            rgb_path = os.path.join(train_image_dir, c_img_id)
+            c_img = imread(rgb_path)
+            c_mask = masks_as_image(c_masks['EncodedPixels'].values)
+            if params.img_scaling is not None:
+                c_img = c_img[::params.img_scaling[0], ::params.img_scaling[1]]
+                c_mask = c_mask[::params.img_scaling[0], ::params.img_scaling[1]]
+            out_rgb += [c_img]
+            out_mask += [c_mask]
+            if len(out_rgb)>=params.batch_size:
+                yield np.stack(out_rgb, 0)/255.0, np.stack(out_mask, 0)
+                out_rgb, out_mask=[], []
+
+def create_aug_gen(in_gen, seed = None):
+    np.random.seed(seed if seed is not None else np.random.choice(range(9999)))
+    for in_x, in_y in in_gen:
+        seed = np.random.choice(range(9999))
+        # keep the seeds syncronized otherwise the augmentation to the images is different from the masks
+        g_x = image_gen.flow(255*in_x,
+                             batch_size = in_x.shape[0],
+                             seed = seed,
+                             shuffle=True)
+        g_y = label_gen.flow(in_y,
+                             batch_size = in_x.shape[0],
+                             seed = seed,
+                             shuffle=True)
+
+        yield next(g_x)/255.0, next(g_y)
+
+def conv_down(filter_, in_layer, name, kernel=(3, 3), activation='relu', padding='same'):
+    l = Conv2D(filter_, kernel, activation=activation, padding=padding, name=name+'_conv1')(in_layer)
+    l = Conv2D(filter_, kernel, activation=activation, padding=padding, name=name+'_conv2')(l)
+    return l
+
+def pool(in_layer, name, pool_size=(2, 2)):
+    return MaxPooling2D(pool_size, name=name+'_pool')(in_layer)
+
+def conv_up(filter_, in_layer, conv_down_layer, name, upsample_size=(2, 2), kernel=(3, 3), activation='relu', padding='same'):
+    l = UpSampling2D(upsample_size, name=name+'_upsample')(in_layer)
+    l = concatenate([l, conv_down_layer], name=name+'_concat')
+    l = Conv2D(filter_, kernel, activation=activation, padding=padding, name=name+'_conv1')(l)
+    l = Conv2D(filter_, kernel, activation=activation, padding=padding, name=name+'_conv2')(l)
+    return l
+
+def dice_coef(y_true, y_pred, smooth=1):
+    intersection = K.sum(y_true * y_pred, axis=[1,2,3])
+    union = K.sum(y_true, axis=[1,2,3]) + K.sum(y_pred, axis=[1,2,3])
+    return K.mean( (2. * intersection + smooth) / (union + smooth), axis=0)
+
+# Plot loss history
+def show_loss(loss_history):
+    epich = np.cumsum(np.concatenate(
+        [np.linspace(0.5, 1, len(mh.epoch)) for mh in loss_history]))
+    fig, (ax1, ax2, ax3, ax4) = plt.subplots(1, 4, figsize=(22, 10))
+    _ = ax1.plot(epich,
+                 np.concatenate([mh.history['loss'] for mh in loss_history]),
+                 'b-',
+                 epich, np.concatenate(
+            [mh.history['val_loss'] for mh in loss_history]), 'r-')
+    ax1.legend(['Training', 'Validation'])
+    ax1.set_title('Loss')
+
+    _ = ax2.plot(epich, np.concatenate(
+        [mh.history['true_positive_rate'] for mh in loss_history]), 'b-',
+                     epich, np.concatenate(
+            [mh.history['val_true_positive_rate'] for mh in loss_history]),
+                     'r-')
+    ax2.legend(['Training', 'Validation'])
+    ax2.set_title('True Positive Rate\n(Positive Accuracy)')
+
+    _ = ax3.plot(epich, np.concatenate(
+        [mh.history['binary_accuracy'] for mh in loss_history]), 'b-',
+                     epich, np.concatenate(
+            [mh.history['val_binary_accuracy'] for mh in loss_history]),
+                     'r-')
+    ax3.legend(['Training', 'Validation'])
+    ax3.set_title('Binary Accuracy (%)')
+
+    _ = ax4.plot(epich, np.concatenate(
+        [mh.history['dice_coef'] for mh in loss_history]), 'b-',
+                     epich, np.concatenate(
+            [mh.history['val_dice_coef'] for mh in loss_history]),
+                     'r-')
+    ax4.legend(['Training', 'Validation'])
+    ax4.set_title('DICE')
